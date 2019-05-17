d56657a3f3126bdcd5d08786f03d8223291271c5
Andy Freeland <andy@goodrx.com>
2019-05-17 01:19:03 -0700

    Add codemod to remove pytest.mark.no_py3 from failing tests

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ running on Python 3 and will fail the test if they pass unexpectedly.
 Usage
 -----
 
+### Adding `@pytest.mark.no_py3`
+
 1. Install this package in your test dependencies.
 
 2. Run your tests on Python 3, savings the results with pytest's `--result-log`
@@ -30,6 +32,37 @@ Usage
 
    ```sh
    python -m pytest_mark_no_py3.add --result-log=path/to/result-log.txt path/to/your/tests
+   ```
+
+   If you're feeling confident, you can also use the `--no-interactive` option
+   to apply the codemod without prompting to accept changes.
+
+### Removing `@pytest.mark.no_py3`
+
+1. Fix some code so that tests start passing on Python 3, violating the xfail.
+
+2. Run your tests on Python 3, savings the results with pytest's `--result-log`
+   option, e.g.:
+
+   ```sh
+   tox -e py37 -- --result-log=test-results.txt
+   ```
+
+   **Warning:** The only failing tests should be the tests that you fixed and
+   want the marker removed from!
+
+3. Install this package with the `bowler` extras in a Python 3 virtualenv:
+
+   ```sh
+   pip install pytest_mark_no_py3[bowler]
+   ```
+
+4. Remove the marker from all of the tests that "failed" (i.e. were expected to
+   fail but actually passed) on Python 3, running from the same directory that
+   you ran the tests from:
+
+   ```sh
+   python -m pytest_mark_no_py3.remove --result-log=path/to/result-log.txt path/to/your/tests
    ```
 
    If you're feeling confident, you can also use the `--no-interactive` option
--- tests/test_bowler.py
+++ tests/test_bowler.py
@@ -4,9 +4,94 @@ from bowler import Query
 import pytest
 
 from pytest_mark_no_py3._bowler import add_marker
+from pytest_mark_no_py3._bowler import filter_already_marked
 from pytest_mark_no_py3._bowler import filter_failing_tests
 from pytest_mark_no_py3._bowler import filter_not_already_marked
+from pytest_mark_no_py3._bowler import remove_marker
 from pytest_mark_no_py3._bowler import TestFunc
+
+# Tuples of (code without marker, code with marker)
+MODIFIER_TEST_CASES = [
+    (
+        dedent(
+            """\
+            def top_level():
+                pass
+            """
+        ),
+        dedent(
+            """\
+            @pytest.mark.no_py3
+            def top_level():
+                pass
+            """
+        ),
+    ),
+    (
+        dedent(
+            """\
+            class Outer(object):
+                class Inner(object):
+                    def method_1(self):
+                        pass
+                    def method_2(self):
+                        pass
+            """
+        ),
+        dedent(
+            """\
+            class Outer(object):
+                class Inner(object):
+                    @pytest.mark.no_py3
+                    def method_1(self):
+                        pass
+                    @pytest.mark.no_py3
+                    def method_2(self):
+                        pass
+            """
+        ),
+    ),
+    (
+        dedent(
+            """\
+            @something()
+            @pytest.mark.another
+            def decorated():
+                pass
+            """
+        ),
+        dedent(
+            """\
+            @something()
+            @pytest.mark.another
+            @pytest.mark.no_py3
+            def decorated():
+                pass
+            """
+        ),
+    ),
+    (
+        dedent(
+            """\
+                pytestmark = pytest.mark.db
+
+
+                def test_it():
+                    pass
+                """
+        ),
+        dedent(
+            """\
+                pytestmark = pytest.mark.db
+
+
+                @pytest.mark.no_py3
+                def test_it():
+                    pass
+                """
+        ),
+    ),
+]
 
 
 class TestTestFunc:
@@ -33,90 +118,7 @@ class TestTestFunc:
         assert TestFunc.from_pytest_results_line(line) == expected
 
 
-@pytest.mark.parametrize(
-    "src, expected",
-    [
-        (
-            dedent(
-                """\
-            def top_level():
-                pass
-            """
-            ),
-            dedent(
-                """\
-            @pytest.mark.no_py3
-            def top_level():
-                pass
-            """
-            ),
-        ),
-        (
-            dedent(
-                """\
-            class Outer(object):
-                class Inner(object):
-                    def method_1(self):
-                        pass
-                    def method_2(self):
-                        pass
-            """
-            ),
-            dedent(
-                """\
-            class Outer(object):
-                class Inner(object):
-                    @pytest.mark.no_py3
-                    def method_1(self):
-                        pass
-                    @pytest.mark.no_py3
-                    def method_2(self):
-                        pass
-            """
-            ),
-        ),
-        (
-            dedent(
-                """\
-            @something()
-            @pytest.mark.another
-            def decorated():
-                pass
-            """
-            ),
-            dedent(
-                """\
-            @something()
-            @pytest.mark.another
-            @pytest.mark.no_py3
-            def decorated():
-                pass
-            """
-            ),
-        ),
-        (
-            dedent(
-                """\
-                pytestmark = pytest.mark.db
-
-
-                def test_it():
-                    pass
-                """
-            ),
-            dedent(
-                """\
-                pytestmark = pytest.mark.db
-
-
-                @pytest.mark.no_py3
-                def test_it():
-                    pass
-                """
-            ),
-        ),
-    ],
-)
+@pytest.mark.parametrize("src, expected", MODIFIER_TEST_CASES)
 def test_add_marker(src, expected, tmpdir):
     pyfile = tmpdir.join("t.py")
     pyfile.write(src)
@@ -125,6 +127,28 @@ def test_add_marker(src, expected, tmpdir):
         Query([str(pyfile)])
         .select("funcdef")
         .modify(add_marker)
+        .execute(interactive=False, write=True, silent=False, in_process=True)
+    )
+
+    modified = pyfile.read()
+    assert modified == expected
+
+
+@pytest.mark.parametrize(
+    "src, expected",
+    [
+        (code_with_marker, code_without_marker)
+        for code_without_marker, code_with_marker in MODIFIER_TEST_CASES
+    ],
+)
+def test_remove_marker(src, expected, tmpdir):
+    pyfile = tmpdir.join("t.py")
+    pyfile.write(src)
+
+    (
+        Query([str(pyfile)])
+        .select("funcdef")
+        .modify(remove_marker)
         .execute(interactive=False, write=True, silent=False, in_process=True)
     )
 
@@ -149,7 +173,7 @@ def filtered_testfuncs(src_file, filter):
     return results
 
 
-def test_filter_already_marked(testdir):
+def test_filter_not_already_marked(testdir):
     pyfile = testdir.makepyfile(
         dedent(
             """\
@@ -185,6 +209,43 @@ def test_filter_already_marked(testdir):
     ]
 
     assert filtered_testfuncs(pyfile, filter_not_already_marked) == expected_testfuncs
+
+
+def test_filter_already_marked(testdir):
+    pyfile = testdir.makepyfile(
+        dedent(
+            """\
+        def test_will_be_marked():
+            pass
+
+        @pytest.mark.another
+        def test_another_that_will_be_marked():
+            pass
+
+        @a_decorator()
+        @b_decorator
+        def test_multiple_decorators_will_be_marked():
+            pass
+
+        @pytest.mark.no_py3
+        def test_already_marked():
+            pass
+
+        @a_decorator()
+        @pytest.mark.no_py3
+        @another_decorator
+        def test_already_marked_many_decorators():
+            pass
+        """
+        )
+    )
+
+    expected_testfuncs = [
+        TestFunc(str(pyfile), "test_already_marked"),
+        TestFunc(str(pyfile), "test_already_marked_many_decorators"),
+    ]
+
+    assert filtered_testfuncs(pyfile, filter_already_marked) == expected_testfuncs
 
 
 def test_filter_failing_tests(testdir):
